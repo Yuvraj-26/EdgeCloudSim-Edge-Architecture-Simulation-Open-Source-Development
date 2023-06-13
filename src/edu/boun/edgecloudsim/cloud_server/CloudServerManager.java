ee76fa8c651cf5f326218c2a452703bcb937c78a
Yuvraj Mahida <2604yuvraj@gmail.com>
2023-06-14 00:18:47 +0100

    Initialised required public class for CloudServerManager

--- src/edu/boun/edgecloudsim/cloud_server/CloudServerManager.java
+++ src/edu/boun/edgecloudsim/cloud_server/CloudServerManager.java
@@ -1,7 +1,7 @@
 /*
  * Title:        EdgeCloudSim - CloudServerManager
- * 
- * Description: 
+ *
+ * Description:
  * CloudServerManager is responsible for creating and terminating
  * the cloud datacenters which operates the hosts and VMs.
  * It also provides the list of VMs running on the hosts and
@@ -9,13 +9,14 @@
  *
  * Please note that, EdgeCloudSim is built on top of CloudSim
  * Therefore, all the computational units are handled by CloudSim
- * 
+ *
  * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
  * Copyright (c) 2017, Bogazici University, Istanbul, Turkey
  */
 
 package edu.boun.edgecloudsim.cloud_server;
 
+//Import required libraries
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,48 +24,49 @@ import org.cloudbus.cloudsim.Datacenter;
 import org.cloudbus.cloudsim.Host;
 import org.cloudbus.cloudsim.VmAllocationPolicy;
 
+//Public class for CloudServerManager
 public abstract class CloudServerManager {
 	protected Datacenter localDatacenter;
 	protected List<List<CloudVM>> vmList;
-
+	//Initilaise CloudServerManager
 	public CloudServerManager() {
 		vmList = new ArrayList<List<CloudVM>>();
 	}
-
+	//initialize Cloud VM
 	public List<CloudVM> getVmList(int hostId){
 		return vmList.get(hostId);
 	}
-	
+	//Initialise Datacenter
 	public Datacenter getDatacenter(){
 		return localDatacenter;
 	}
-	
+
 	/*
-	 * initialize edge server manager if needed
+	 *Initialize edge server manager if required
 	 */
 	public abstract void initialize();
 
 	/*
-	 * provides abstract Vm Allocation Policy for Cloud Datacenters
+	 *Provides abstract Vm Allocation Policy for Cloud Datacenters
 	 */
 	public abstract VmAllocationPolicy getVmAllocationPolicy(List<? extends Host> list, int dataCenterIndex);
 
 	/*
-	 * Starts Datacenters
+	 *Starts Datacenters
 	 */
 	public abstract void startDatacenters() throws Exception;
-	
+
 	/*
-	 * Terminates Datacenters
+	 *Terminates Datacenters
 	 */
 	public abstract void terminateDatacenters();
 	/*
-	 * Creates VM List
+	 *Creates VM List
 	 */
 	public abstract void createVmList(int brokerId);
-	
+
 	/*
-	 * returns average utilization of all VMs
+	 *Returns average utilization of all VMs
 	 */
 	public abstract double getAvgUtilization();
 }
