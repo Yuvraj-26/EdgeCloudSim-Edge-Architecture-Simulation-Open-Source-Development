693811f3877c58d9a60054616a91e7983a7ce04f
Yuvraj Mahida <2604yuvraj@gmail.com>
2023-06-14 01:09:47 +0100

    Normadic Mobility module is fine, Mobility model modified to remove default Constructor that creates an empty MobilityModel

--- src/edu/boun/edgecloudsim/mobility/MobilityModel.java
+++ src/edu/boun/edgecloudsim/mobility/MobilityModel.java
@@ -1,12 +1,12 @@
 /*
  * Title:        EdgeCloudSim - Mobility Model
- * 
- * Description: 
+ *
+ * Description:
  * MobilityModel is an abstract class which is used for calculating the
  * location of each mobile devices with respect to the time. For those who
  * wants to add a custom Mobility Model to EdgeCloudSim should extend
  * this class and provide a concrete instance via ScenarioFactory
- *               
+ *
  * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
  * Copyright (c) 2017, Bogazici University, Istanbul, Turkey
  */
@@ -18,23 +18,23 @@ import edu.boun.edgecloudsim.utils.Location;
 public abstract class MobilityModel {
 	protected int numberOfMobileDevices;
 	protected double simulationTime;
-	
+
 	public MobilityModel(int _numberOfMobileDevices, double _simulationTime){
 		numberOfMobileDevices=_numberOfMobileDevices;
 		simulationTime=_simulationTime;
 	};
-	
+
 	/*
 	 * Default Constructor: Creates an empty MobilityModel
 	 */
-	public MobilityModel() {
-	}
+	//public MobilityModel() {
+	//}
 
 	/*
 	 * calculate location of the devices according to related mobility model
 	 */
 	public abstract void initialize();
-	
+
 	/*
 	 * returns location of a device at a certain time
 	 */
